6ef7314c5fbc500d19ae96748dc641f87bfe7127
umarndungo <omarnjagi@gmail.com>
2026-03-30 15:52:18 +0300

    Kotlin: Added Conditionals.

--- src/conditionals.kt
+++ src/conditionals.kt
@@ -1,5 +1,5 @@
 fun conditionals(){
-    val finishedHomework = true
+    /*val finishedHomework = true
     val i = 1 // or 5
     if (finishedHomework) {
         println("Can go to the cinema")
@@ -41,9 +41,55 @@ fun conditionals(){
         println("Yes")
     } else {
         println("What?")
+    }*/
+
+    println("Is it going to rain?")
+    val probability = 70
+        when {
+            probability < 40 -> println("Unlikely")
+            probability <= 80 -> println("Likely")
+            probability < 100 -> println("Yes")
+            else -> println("What?")
+        }
+
+    val text = when {
+            probability < 40 -> "Unlikely"
+            probability <= 80 -> "Likely"
+            probability < 100 -> "Yes"
+            else -> "What?"
+            }
+    println(text)
+
+    val number = 1 // or 2, 3, 4, 5, 6
+    when (number) {
+        1 -> {
+        println("Missed hit")
+        }
+        2, 3, 4, 5 -> {
+        println("Hit with value $number")
+        }
+        6 -> {
+        println("Critical hit")
+        }
     }
 
 
+    val text2 = when (number) {
+            1 -> "Missed hit"
+            in 2..5 -> "Hit with value $number"
+            6 -> "Critical hit"
+            else -> "Unsupported value"
+    }
+    println(text2)
+
+    val something: Any = "ABC" // or 123, 0.1, true
+    when (something) {
+        is String -> println("This is String")
+        is Int -> println("This is Int")
+        is Double -> println("This is Double")
+        is Boolean -> println("This is Boolean")
+    }
+    println(something)
 
 
 }
