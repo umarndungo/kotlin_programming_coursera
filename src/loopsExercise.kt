4c825c5db931366ead9aeabeb72446541bdd7519
umarndungo <omarnjagi@gmail.com>
2026-04-07 10:36:34 +0300

    Practice: Inverted Reflected Triangle.

--- src/loopsExercise.kt
+++ src/loopsExercise.kt
@@ -11,4 +11,40 @@ fun loopsExercise(){
         }
         println()
     }
+
+    println("\nAn inverted Isosceles Triangle")
+    for (i in 1..5){
+        val numberOfSpaces = i - 1
+        for (j in 1..numberOfSpaces){
+            print(" ")
+        }
+        val numberOfStars = 11 - i * 2
+        for (j in 1 ..numberOfStars){
+            print("*")
+        }
+        println()
+    }
+    println("\nA Triangle ending an one beginning below it")
+    for (i in 1 until 5) {
+        val numberOfSpaces = 5 - i
+        for (j in 1..numberOfSpaces) {
+            print(" ")
+        }
+        val numberOfStars = i
+        for (j in 1..numberOfStars) {
+            print("*")
+        }
+        println()
+    }
+    for (i in 1..5) {
+        val numberOfSpaces = i - 1
+        for (j in 1..numberOfSpaces) {
+            print(" ")
+        }
+        val numberOfStars = 6 - i
+        for (j in 1..numberOfStars) {
+            print("*")
+        }
+        println()
+    }
 }
